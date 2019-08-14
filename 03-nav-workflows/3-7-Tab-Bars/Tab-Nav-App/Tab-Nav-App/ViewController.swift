972b14b7e483edf6096da17503e8a9c6792fc970
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-08-14 11:38:33 +0500

    Add 3.72 - Tab bar with navigation controllers

---
+++ 03-nav-workflows/3-7-Tab-Bars/Tab-Nav-App/Tab-Nav-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tab-Nav-App
+//
+//  Created by Saad Mashkoor Siddiqui on 14/08/2019.
+//  Copyright © 2019 Saad Mashkoor Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
