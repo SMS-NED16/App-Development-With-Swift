0e7de2378f629e201a33dafce36cb4e17c6085a7
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-08-17 06:11:52 +0500

    Add 4.2 - App Delegate methods demo

---
+++ 04-Tables-Persistence/4-2-App-Lifecycle/App Lifecycle/App Lifecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Lifecycle
+//
+//  Created by Saad Mashkoor Siddiqui on 17/08/2019.
+//  Copyright © 2019 Saad Mashkoor Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
