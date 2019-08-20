6fdf608e646baaa753acd31f0a6cb9b99a592b3f
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-08-20 05:23:07 +0500

    Lab 4.3 - Add code to handle navigation from table to form and managing returned athlete

--- 04-Tables-Persistence/4-3-mvc/Lab - MVC/lab/FavoriteAthlete/FavoriteAthlete/AthleteTableViewController.swift
+++ 04-Tables-Persistence/4-3-mvc/Lab - MVC/lab/FavoriteAthlete/FavoriteAthlete/AthleteTableViewController.swift
@@ -40,5 +40,40 @@ class AthleteTableViewController: UITableViewController {
 
     }
  */
-
+    // Casting segue destination from UIViewController to AthleteFormViewController
+    // Each row in the table has an index which we can use to get a path that describes
+    // how to get to that row in the table.
+    // If the segue being triggered is one of `edit athlete` i.e. it has been triggered
+    // by clicking on an existing cell, then use the index path to get the selected athlete
+    // and load it into the athleteFormViewController before the segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Check if the segue is to edit an existing athlete
+        let athleteFormViewController = segue.destination as! AthleteFormViewController
+        if let indexPath = tableView.indexPathForSelectedRow,
+            segue.identifier == PropertyKeys.editAthleteSegue {
+            athleteFormViewController.athlete = athletes[indexPath.row]
+        }
+    }
+    
+    // IB Action that handles data being returned from the form view controller
+    @IBAction func updateAthletesTable(segue: UIStoryboardSegue) {
+        // Cast the segue's source as an AthleteFormViewController
+        let segueSource = segue.source as! AthleteFormViewController
+        let athlete = segueSource.athlete!      // force unwrapping - not best idea
+        
+        // Find the index path in the table view controller that was selected before
+        // going to the segue's source. If this is unwrapped successfully, the user
+        // clicked on an existing athlete to edit its details, so find the athlete
+        // using its index path, replace it with the edited athlete
+        if let indexPath = tableView.indexPathForSelectedRow {
+            athletes.remove(at: indexPath.row)
+            athletes.insert(athlete, at: indexPath.row)
+            tableView.deselectRow(at: indexPath, animated: true)
+        } else {
+            // if cannot unwrap the indexPath, did not select an existing athlete
+            // which means segue was done to add a new athlete
+            // so append returned athlete to end of list
+            athletes.append(athlete)
+        }
+    }
 }
