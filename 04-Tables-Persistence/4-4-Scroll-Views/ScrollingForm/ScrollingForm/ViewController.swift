b8c89451ef9e4a8fada84dce899c12c6a776690f
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-08-20 07:00:56 +0500

    4.4 - Add notification observers to adjust scroll view size when keyboard rendered/dismissed

--- 04-Tables-Persistence/4-4-Scroll-Views/ScrollingForm/ScrollingForm/ViewController.swift
+++ 04-Tables-Persistence/4-4-Scroll-Views/ScrollingForm/ScrollingForm/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        registerForKeyboardNotifications()
     }
 
+    // Adds notification observers ot the Notification Center to detect every time the
+    // keyboard is drawn or dismissed - calls a callback function to adjust scroll view size accordingly
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)),
+                                               name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc func keyboardWasShown(_ notification: NSNotification) {
+        guard let info = notification.userInfo,
+            let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue
+            else { return }
+        
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
 
 }
 
