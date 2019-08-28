0d3c72769d969400276332d061841fafba167dfc
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-08-28 06:25:38 +0500

    4.4 - Add base prj for ISpy zoomable img scroll view

---
+++ 04-Tables-Persistence/4-4-Scroll-Views/ISpy/ScrollViewISpy/ScrollViewISpy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ScrollViewISpy
+//
+//  Created by Saad Mashkoor Siddiqui on 28/08/2019.
+//  Copyright © 2019 Saad Mashkoor Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK:- IB Outlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imgView: UIImageView!
+    
+}
+
